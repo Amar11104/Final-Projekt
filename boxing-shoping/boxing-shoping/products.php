35b2e4ec2e75a2789c631ccff8a1d7c82fdd0521
Amar11104 <amar.dushulovci@gmail.com>
2025-05-28 23:12:17 +0200

    lisi projekt

---
+++ boxing-shoping/boxing-shoping/products.php
@@ -0,0 +1,325 @@
+<?php
+    session_start();
+    require 'check_if_added.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>Projectworlds Store</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+            <?php
+                require 'header.php';
+            ?>
+            <div class="container">
+                <div class="jumbotron">
+                    <h1>Welcome to our Boxing Store!</h1>
+                    <p>We have the best Gloves, shoes and headgear for you. No need to hunt around, we have all in one place.</p>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/venum.jpg" alt="venum">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Venum </h3>
+                                    <p>Price: 100$</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(1)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=1" class="btn btn-block btn-primary" name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                    
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/venum1.jpg" alt="venum">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Venum red-black</h3>
+                                    <p>Price: 75$</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(2)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=2" class="btn btn-block btn-primary" name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/addidas.jpg" alt="Addidas ">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Addidas </h3>
+                                    <p>Price: 140$</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(3)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=3" class="btn btn-block btn-primary" name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/sting.jpeg" alt="Olympus">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Sting</h3>
+                                    <p>Price: 120$</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(4)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=4" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/metal.jpg" alt="metal">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Metal</h3>
+                                    <p>Price: 45$</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(5)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=5" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/reyes.jpg" alt="reyes ">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Reyes</h3>
+                                    <p>Price: 200$</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(6)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=6" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/reyes1.JPG" alt="reyes">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Reyes</h3>
+                                    <p>Price: 150$</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(7)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=7" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/eliteshoes.jpg" alt=" eliteshoes">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>EliteShoes</h3>
+                                    <p>Price: 120$</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(8)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=8" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/shoesvenum.jpg" alt=" venumshoes">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>VenumShoes</h3>
+                                    <p>Price: 150</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(9)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=9" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/headgear.jpg" alt="headgear ">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Headgear</h3>
+                                    <p>Price: 70$</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(10)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=10" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                   
+                   
+                    </div>
+                </div>
+            </div>
+            <br><br><br><br><br><br><br><br>
+           <footer class="footer">
+               <div class="container">
+                <center>
+                   <p>Copyright &copy Boxing-shoping</a> Store. All Rights Reserved.</p>
+                   <p>This website is developed by Lis Salihu</p>
+               </center>
+               </div>
+           </footer>
+        </div>
+    </body>
+</html>
